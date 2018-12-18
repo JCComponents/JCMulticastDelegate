10e0023bdefc86ed6e4f5d4e04d4b4d87dd99181
lswhcmcc <411863122@qq.com>
2018-12-18 19:34:18 +0800

    Update JCAppDelegate.h

--- Example/JCMulticastDelegate/JCAppDelegate.h
+++ Example/JCMulticastDelegate/JCAppDelegate.h
@@ -2,8 +2,8 @@
 //  JCAppDelegate.h
 //  JCMulticastDelegate
 //
-//  Created by lswhcmcc on 05/16/2018.
-//  Copyright (c) 2018 lswhcmcc. All rights reserved.
+//  Created by JC on 05/16/2018.
+//  Copyright (c) 2018 lswh. All rights reserved.
 //
 
 @import UIKit;
